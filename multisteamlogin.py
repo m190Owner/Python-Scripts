35b661d7083430b582cf605894e15ea0924a05f5
m190Owner <120240509+m190Owner@users.noreply.github.com>
2022-12-10 19:48:59 +1300

    Update and rename steamlog.py to multisteamlogin.py

--- multisteamlogin.py
+++ multisteamlogin.py
@@ -1,19 +1,22 @@
+#This script allows you to login with multiple steam accounts.
+#Mainly used for checking if accounts are valid.
+
 import selenium
 from selenium import webdriver
 
 #create list of usernames and passwords
-usernames = ["its_talesd", "hjgyjgygy", "hypapumynany"]
-passwords = ["HvfnvSL2", "Ezxpl8Ub", "DnE1OGBq"]
+usernames = ["user 1", "user 2", "user 3"]
+passwords = ["Pass 1", "Pass 2", "Pass 3"]
 
 #open web browser and navigate to Steam login page
 browser = webdriver.Chrome()
 browser.get("https://store.steampowered.com/login/")
 
 #iterate through usernames and passwords and attempt login
-        for username, password in zip(usernames, passwords):
-        browser.find_element_by_id("input_username").send_keys(username)
-        browser.find_element_by_id("input_password").send_keys(password)
-        browser.find_element_by_id("login_btn_signin").click()
+for username, password in zip(usernames, passwords):
+browser.find_element_by_id("input_username").send_keys(username)
+browser.find_element_by_id("input_password").send_keys(password)
+browser.find_element_by_id("login_btn_signin").click()
 
 Copy code
 #check if login was successful
