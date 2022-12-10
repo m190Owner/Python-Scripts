133c82d47afa174941455071758c60b95b50d793
m190Owner <120240509+m190Owner@users.noreply.github.com>
2022-12-10 20:01:53 +1300

    Add files via upload

---
+++ friendmaker.py
@@ -0,0 +1,19 @@
+# Import the necessary libraries
+import webbrowser
+import requests
+
+# Set the URL of the website that you want to visit
+url = "https://www.example.com"
+
+# Open the website in a new tab of the default web browser
+webbrowser.open_new_tab(url)
+
+# Set the URL of the file that you want to download
+file_url = "https://www.example.com/files/myfile.txt"
+
+# Download the file
+response = requests.get(file_url)
+
+# Save the file to the local filesystem
+with open("myfile.txt", "w") as file:
+    file.write(response.text)
